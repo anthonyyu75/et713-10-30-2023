a17593b96b0e828eae73fb9bbf7f1459bea4402e
EC2 Default User <ec2-user@ip-10-16-10-107.ec2.internal>
2023-10-30 20:00:11 +0000

    new

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideosComponent } from './videos/videos.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'videos', component: VideosComponent},
+{ path: '', component: ProductsComponent}
+]
+;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
